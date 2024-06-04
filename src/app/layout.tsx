fba773be602f821a38272d5531538a8a542655aa
gabriella-guimaraes <gabigara13@gmail.com>
2024-06-04 17:04:25 -0300

    feat: Home Page component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/Components/navbar/Navbar";
+import Footer from "@/Components/footer/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,7 @@ export default function RootLayout({
         <div className="container">
           <Navbar />
           {children}
-
+          <Footer />
         </div>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
 import Image from "next/image";
+import styles from "./page.module.css";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div>Hello there</div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Hub</h1>
+        <p className={styles.desc}>O Creative Hub é o lugar onde mentes criativas se encontram. Compartilhe suas ideias e colabore para construir algo incrível.</p>
+
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+
+        <div className={styles.brands}>
+          <Image className={styles.brandImg} src="/brands.png" fill alt="brands logos" />
+        </div>
+
+      </div>
+
+      <div className={styles.imgContainer}>
+          <Image className={styles.heroImg} src="/hero.gif" fill unoptimized alt="Creative Hub Hero gif" />
+      </div>
+      
+    </div>
   );
 }
+
+export default Home;
